f6b290277860c01982d6b2822be4e43f05c8f4a8
SergeySalimov <salimov_s@tut.by>
2020-02-24 14:20:55 +0300

    feat(all project): add webpack to project => ready to start

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.scss';
+import './assets/img/opera_2019-12-04_21-59-08.png';
+import './assets/img/sunlight-forest-nature.jpg';
+import './assets/img/fircone-white32x32.png';
+
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const DIR_FROM = 'src';
+const DIR_TO = 'dist';
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+module.exports = {
+  context: path.resolve(__dirname, DIR_FROM),
+  mode: 'development',
+  entry: {
+    main: './index.js',
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@css': path.resolve(__dirname, 'src/css'),
+      '@js': path.resolve(__dirname, 'src/css'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  output: {
+    // [name], [contenthash]
+    filename: '[hash].bundle.js',
+    path: path.resolve(__dirname, DIR_TO),
+    publicPath: '/',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, DIR_FROM),
+    port: 4200,
+    open: 'chrome',
+    historyApiFallback: true,
+    // historyApiFallback: {
+    //   index: 'index.html',
+    // },
+    // hot: isDev,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      // filename: 'style.[hash].css',
+      filename: '[name].css',
+      // chunkFilename: '[id].css',
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, `${DIR_FROM}/favicon.ico`),
+        to: path.resolve(__dirname, DIR_TO),
+      },
+    ]),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+      {
+        test: /\.handlebars$/,
+        loader: 'handlebars-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            hmr: isDev,
+            reloadAll: true,
+          },
+        }, 'css-loader',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif|ico)$/,
+        use: ['file-loader?name=images/[name].[ext]'],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader?name=font/[name].[ext]'],
+      },
+    ],
+  },
+};
