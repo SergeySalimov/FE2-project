85ef3ae8050e3992c1f1ca85ae5313de2e01453c
SergeySalimov <salimov_s@tut.by>
2020-02-27 15:56:30 +0300

    feat(all project): add jquery-ui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap';
 import './css/style.scss';
 import './assets/img/oak-sm.png';
 import './assets/img/cart2-sm.png';
 import './assets/img/sunlight-forest-nature.jpg';
 import './assets/img/opera_2019-12-04_21-59-08.png';
 import './assets/img/images.png';
+// import jquery-ui library
+import "../node_modules/jquery-ui/themes/base/all.css";
+import "../node_modules/jquery-ui/themes/base/theme.css";
+// import 'jquery-ui/ui/widgets/dialog';
+// import 'jquery-ui/ui/widgets/button';
+import 'jquery-ui/ui/widgets/tabs';
 
+
+// const tabs = $('#tabs');
+
+$('#tabs').tabs();
+
+// const opt = {
+//   autoOpen: false,
+//   modal: true,
+//   width: 200,
+//   height: 150,
+//   title: 'Modal'
+// };
+//
+// const theDialog = $('#dialog').dialog(opt);
+//
+// $('#button').button();
+//
+// $('#button').click(() => {
+//   theDialog.dialog('open');
+// });
